14d010a8972ec9cc2576700a470603bc5b6cb507
zzhannah <60711314+zzhannah@users.noreply.github.com>
2020-04-07 20:15:50 -0700

    Update Base.java

--- Base.java
+++ Base.java
@@ -1,16 +1,16 @@
 /******************************************************************************
-  						                                	Han Zhang
-					                                		CSE 12, Winter 2020
-				                                			March 21st, 2020
-					                                		cs12wi20kj
-			            	Assignment TEN
+							Han Zhang
+							CSE 12, Winter 2020
+							March 21st, 2020
+							cs12wi20kj
+				Assignment TEN
  File Name:	Base.java
  Description:	The file contains abstract Base and its two methods, getName
  	        	and isGreaterThan. Used in Heap.java and Driver.java.
  Public Functions:
- 	        getName		    - get name from a UCSDStduent, used in Driver.java
-	        isGreaterThan	- compare two student name and define if is greater, 
-			                used in Driver.java
+ 	getName		- get name from a UCSDStduent, used in Driver.java
+	isGreaterThan	- compare two student name and define if is greater, 
+			used in Driver.java
 ******************************************************************************/
 public abstract class Base {
 	public String getName () {
